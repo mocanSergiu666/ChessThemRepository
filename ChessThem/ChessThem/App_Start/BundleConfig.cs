9ebb87a335e0ff0e5e8964db82386376a75b9bf0
Mocan Sergiu <mocanSergiu666@gmail.com>
2017-03-15 22:02:07 +0200

    Initializes the project. Updates all the libraries. Installs new ones needed.
    Signed-off-by: Mocan Sergiu <mocanSergiu666@gmail.com>

---
+++ ChessThem/ChessThem/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace ChessThem
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/scripts/jquery").Include("~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/scripts/jquery-validate").Include("~/Scripts/jquery.validate*"));
+			// Use the development version of Modernizr to develop with and learn from. Then, when you're
+			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+			bundles.Add(new ScriptBundle("~/scripts/modernizr").Include("~/Scripts/modernizr-*"));
+			bundles.Add(new ScriptBundle("~/scripts/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
+
+			bundles.Add(new StyleBundle("~/styles/bootstrap").Include("~/Content/bootstrap.css"));
+		}
+	}
+}
---
+++ ChessThem/ChessThem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChessThem.MvcApplication" Language="C#" %>
